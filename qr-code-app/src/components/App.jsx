757e01d7c7ef8fd47852e0d12d62e02bfa2a8eaa
juanduque68 <hanzduque393@gmail.com>
2021-03-03 18:56:55 -0500

    Se reemplaza la vista de prueba por el generador de código qr y el botón de descarga del qr.

--- qr-code-app/src/components/App.jsx
+++ qr-code-app/src/components/App.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import Tabs from "./Tabs";
+import ScanArea from "./ScanArea.jsx";
+
 import "../style/app.css";
 
-function App() {
-  return (
-    <div>
-      <h1>You can make several qr code</h1>
-      <Tabs>
-        <div label="QR Code 1">
-          See ya later, <em>Alligator</em>!
-        </div>
-        <div label="QR Code 2">
-          After 'while, <em>Crocodile</em>!
-        </div>
-        <div label="QR Code 3">
-          Nothing to see here, this tab is <em>extinct</em>!
-        </div>
-      </Tabs>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "hola mundox",
+      url2: "ho",
+      url3: "lets go",      
+  };
+  }
+  render(){
+
+    return (
+      <div>
+        <h1>You can make several QR code</h1>
+        <Tabs>
+          <div label="QR Code 1">
+            <ScanArea url={ this.state.url }/>
+          </div>
+          <div label="QR Code 2">
+            <ScanArea url={ this.state.url2 }/>          
+          </div>
+          <div label="QR Code 3">
+            <ScanArea url={ this.state.url3 }/>
+          </div>
+        </Tabs>
+      </div>
+    );
+  }
 }
 
 export default App;
