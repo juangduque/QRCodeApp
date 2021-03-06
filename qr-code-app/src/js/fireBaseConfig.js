b02ba238167925f4a0b9e34da92de98334a03107
juanduque68 <hanzduque393@gmail.com>
2021-03-05 22:10:25 -0500

    Se deja la aplicación lista para subir a producción

--- qr-code-app/src/js/fireBaseConfig.js
+++ qr-code-app/src/js/fireBaseConfig.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrD07iXkt-Q9j4-WEzJRVTMSQ0Q4AZ9fM",
-  authDomain: "qrcodeapp-official.firebaseapp.com",
-  projectId: "qrcodeapp-official",
-  storageBucket: "qrcodeapp-official.appspot.com",
+  apiKey: "yourApiKey",
+  authDomain: "yourAuthDomain",
+  projectId: "yourProjectId",
+  storageBucket: "yourStorageBucket",
   messagingSenderId: "284210827483",
-  appId: "1:284210827483:web:d008e6b36da1b5100afcfe"
+  appId: "yourAppId"
 };
 
 export const app = firebase.initializeApp(firebaseConfig);
